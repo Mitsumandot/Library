2fc82f21c16385bad38f3b11120996968a10dcff
Mitsumandot <chiguer.othmane@ensam-casa.ma>
2025-04-05 15:41:12 +0000

    Books creation, remove and update readings

---
+++ script.js
@@ -0,0 +1,86 @@
+const myLibrary = [];
+
+function Book(id, name, author, page, read) {
+    this.name = name;
+    this.id = id;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+    this.info = function () {
+        return this.name + ", " + this.author + ", " + this.page + ", " + 
+        (this.read === true ? "This book is read." : "This book is not read.");
+    };
+
+
+
+}
+
+
+function addBookToLibrary(name, author, page, read) {
+    let newBook = new Book(crypto.randomUUID(), name, author, page, read);
+    myLibrary.push(newBook);
+
+}
+
+Book.prototype.updateRead = function () {
+    this.read = !this.read;
+    
+}
+
+addBookToLibrary("Charlotte", "David Foenkinos", 254, true);
+addBookToLibrary("Le Gai savoir", "Nietzsche", 448, false);
+addBookToLibrary("Crîme et Châtiment", "Fiodor Dostoïevski", 728, false);
+addBookToLibrary("L'Etranger", "Alber Camus", 200, true);
+
+const booksDom = [];
+const removeDom = [];
+
+function display() {
+    let list = document.querySelector(".list");
+    list.textContent = "";
+    for (let i in myLibrary) {
+        let x = document.createElement("div");
+        x.textContent = myLibrary[i].info();
+        x.textContent += "  ";
+        let btn = document.createElement("button");
+        btn.textContent = "Remove";
+        btn.addEventListener("click", () => {
+            myLibrary.splice(i, 1);
+            display();
+        });
+        let toggle = document.createElement("button");
+        toggle.textContent = "read";
+        toggle.addEventListener("click", () => {
+            myLibrary[i].updateRead();
+            display();
+        })
+        x.appendChild(toggle);
+        x.appendChild(btn);
+        list.appendChild(x);
+        
+    }
+}
+
+let button = document.querySelector(".ass");
+let form = document.querySelector("form");
+button.addEventListener("click", () => {
+    form.style["display"] = "flex";
+    button.style["display"] = "none";
+})
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let author = document.getElementById("author").value;
+    let page = document.getElementById("page").value;
+    let read = document.querySelector('input[name="read"]:checked').value;
+    read = read === "yes";
+    addBookToLibrary(name, author, page, read);
+    display();
+    
+})
+
+display();
+
